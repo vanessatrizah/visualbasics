796765fab787cf0434b1f9ec8d6cd3856ae9463b
vanessatrizah <125655979+vanessatrizah@users.noreply.github.com>
2024-03-29 11:48:24 +0300

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,27 +1,26 @@
 ﻿Public Class Form1
-    Dim cropsList As New List(Of String)
-    Private Sub AddCrop_Click(sender As Object, e As EventArgs) Handles AddCrop.Click
-        Dim cropName As String = InputBox("Enter Crop Name:", "Add Crop")
-        Dim variety As String = InputBox("Enter Variety:", "Add Crop")
-        Dim plantingDate As String = InputBox("Enter Planting Date:", "Add Crop")
-        Dim harvestingDate As String = InputBox("Enter Harvesting Date:", "Add Crop")
-        Dim cropDetails As String = $"{cropName}, {variety}, Planting: {plantingDate}, Harvesting: {harvestingDate}"
-        cropsList.Add(cropDetails)
+    Dim livestockList As New List(Of String)
+    Private Sub BtnAddLivestock_Click(sender As Object, e As EventArgs) Handles BtnAddLivestock.Click
+        Dim animalType As String = InputBox("Enter Animal Type:", "Add Livestock")
+        Dim feedingSchedule As String = InputBox("Enter Feeding Schedule:", "Add Livestock")
+        Dim breedingDetails As String = InputBox("Enter Breeding Details:", "Add Livestock")
+        Dim healthMonitoring As String = InputBox("Enter Health Monitoring Details:", "Add Livestock")
+        Dim livestockDetails As String = $"Type: {animalType}, Feeding: {feedingSchedule}, Breeding: {breedingDetails}, Health Monitoring: {healthMonitoring}"
+        livestockList.Add(livestockDetails)
     End Sub
 
-    Private Sub DeleteCrop_Click(sender As Object, e As EventArgs) Handles DeleteCrop.Click
-        Dim indexString As String = InputBox("Enter the index of the crop to delete:", "Delete Crop")
+    Private Sub BtnDeleteLivestock_Click(sender As Object, e As EventArgs) Handles BtnDeleteLivestock.Click
+        Dim indexString As String = InputBox("Enter the index of the livestock to delete:", "Delete Livestock")
         Dim index As Integer
-        If Integer.TryParse(indexString, index) AndAlso index >= 0 AndAlso index < cropsList.Count Then
-            cropsList.RemoveAt(index)
+        If Integer.TryParse(indexString, index) AndAlso index >= 0 AndAlso index < livestockList.Count Then
+            livestockList.RemoveAt(index)
         Else
             MessageBox.Show("Invalid index. Please enter a valid index.")
         End If
-
     End Sub
 
-    Private Sub ViewCrop_Click(sender As Object, e As EventArgs) Handles ViewCrop.Click
-        MessageBox.Show("List of Crops:" & vbCrLf & String.Join(vbCrLf, cropsList))
+    Private Sub BtnViewLivestock_Click(sender As Object, e As EventArgs) Handles BtnViewLivestock.Click
+        MessageBox.Show("List of Livestock:" & vbCrLf & String.Join(vbCrLf, livestockList))
     End Sub
 
     Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
