69a395953b21ae3c03f786dc3ccd44fd50cce6c4
vanessatrizah <125655979+vanessatrizah@users.noreply.github.com>
2024-03-29 09:38:20 +0300

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim cropsList As New List(Of String())
+    Private Sub AddCrop_Click(sender As Object, e As EventArgs) Handles AddCrop.Click
+        Dim cropName As String = InputBox("Enter Crop Name:", "Add Crop")
+        Dim variety As String = InputBox("Enter Variety:", "Add Crop")
+        Dim plantingDate As String = InputBox("Enter Planting Date:", "Add Crop")
+        Dim harvestingDate As String = InputBox("Enter Harvesting Date:", "Add Crop")
+        Dim cropDetails As String = $"{cropName}, {variety}, Planting: {plantingDate}, Harvesting: {harvestingDate}"
+        cropsList.Add(cropDetails)
+    End Sub
+
+    Private Sub DeleteCrop_Click(sender As Object, e As EventArgs) Handles DeleteCrop.Click
+        Dim indexString As String = InputBox("Enter the index of the crop to delete:", "Delete Crop")
+        Dim index As Integer
+        If Integer.TryParse(indexString, index) AndAlso index >= 0 AndAlso index < cropsList.Count Then
+            cropsList.RemoveAt(index)
+        Else
+            MessageBox.Show("Invalid index. Please enter a valid index.")
+        End If
+
+    End Sub
+
+    Private Sub ViewCrop_Click(sender As Object, e As EventArgs) Handles ViewCrop.Click
+        MessageBox.Show("List of Crops:" & vbCrLf & String.Join(vbCrLf, cropsList))
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Me.Close()
+    End Sub
+End Class
